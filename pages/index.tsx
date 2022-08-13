1568834e9c2ff896c4e80b4899fb25618f74f6af
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-08-14 05:21:14 +0530

    ability to add polls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
+import React from "react";
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
 const QuestionCreator: React.FC = () => {
-  const { mutate } = trpc.useMutation("questions.create");
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const client = trpc.useContext();
+
+  const { mutate, isLoading } = trpc.useMutation("questions.create", {
+    onSuccess() {
+      // invalidate queries forces it to refetch the questions when mutation is success
+      client.invalidateQueries("questions.get-all");
+      if (!inputRef.current) return;
+      inputRef.current.value = "";
+    },
+  });
 
   return (
     <input
-      onSubmit={(event) => {
-        console.log();
+      ref={inputRef}
+      disabled={isLoading}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          console.log("Enterrr ", event.currentTarget.value);
+          mutate({ question: event.currentTarget.value });
+          event.currentTarget.value = "";
+        }
       }}
     ></input>
   );
@@ -20,12 +37,19 @@ const Home: NextPage = () => {
 
   console.log(data);
   return (
-    <div>
+    <div className="p-8">
+      <div className="mb-4">
+        <h6 className="text-2xl font-bold mb-4">Add a poll</h6>
+
+        <QuestionCreator />
+      </div>
       <div>
-        <h6>Questions</h6>
-        <div>
+        <h6 className="text-2xl font-bold mb-4">Questions</h6>
+        <div className="flex flex-col gap-4">
           {data?.map((q) => (
-            <div key={q.id}>{q.question}</div>
+            <div className="question" key={q.id}>
+              {q.question}
+            </div>
           ))}
         </div>
       </div>

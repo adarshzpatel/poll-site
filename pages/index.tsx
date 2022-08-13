9a627c362b9bdff32a472c9fef3114c0df4d3f1f
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-08-14 03:53:35 +0530

    added prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from 'next'
+import {prisma} from "../db/client"
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
     <div>
-      
+      <p className='text-4xl'>{props.questions}</p>
     </div>
   )
 }
 
 export default Home
+
+
+export const getServerSideProps = async () => {
+  const questions = await prisma?.pollQuestion.findMany();
+  
+  return {
+    props: {
+      questions:JSON.stringify(questions)
+    }
+  }
+}
